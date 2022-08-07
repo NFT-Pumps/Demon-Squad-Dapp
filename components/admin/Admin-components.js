be81cf0d343b8ed89ce355e4345ac8c928729d39
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-08-06 20:17:09 -0500

    color

--- components/admin/Admin-components.js
+++ components/admin/Admin-components.js
@@ -168,7 +168,7 @@ const AdminComponents = () => {
                   Connect Wallet
                 </a>
                 <Link href={process.env.mainWWW}>
-                  <a className="btn btn-md m-t-30  btn-outline-light " style={{ backgroundColor: "#ce3a7f" }}>
+                  <a className="btn btn-md m-t-30  btn-outline-light " style={{ backgroundColor: "#044022" }}>
                     Back Home
                   </a>
                 </Link>
