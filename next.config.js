9638ee674d8cea917f1f0e478831985718c305d7
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-08-06 21:37:04 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
     defaultGas: 125000,//75000,
     mintType : "Pre-Sale", //Pre-Sale Public
     enforceWhitelist : true,
-    ethValue : "0.06",
+    ethValue : "0.024",
     ethWLValue : "0.00",
     debug: false,
     network: 'mainnet',
@@ -27,7 +27,7 @@ module.exports = {
       discord : "",
       twitter : "https://twitter.com/LFG_Demon"
     },
-    maxMint: 3000,
+    maxMint: 1200,
     maxMintCount: 50,
     messagehash: '0x865f293da4a629f00bd8072be6115031c608fa4a13b819fd087811752fc8bb90',
     signatureBytes: '0x4b7715f34823936b045d1e5ca6c295b7f5ecff3d0c158118c71441ed217c3ddb026617ce49dc15781bdb25c8c34ed4a1f4d68df6080f6dd0004511cf05032bf01c',
