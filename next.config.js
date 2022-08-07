3ce966135fd1d8e519931ee7bb10926ff745b314
greene5952 <96886480+greene5952@users.noreply.github.com>
2022-08-06 19:09:44 -0500

    update

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   reactStrictMode: false,
   analyticsId: '7vsHWbYwcKpA3lFTng2Qe9iNzE7',
   env: {
-    siteTitle: "Farm Folk NFT Minting Dapp",
+    siteTitle: "Demon Squad NFT Minting Dapp",
     contractAddress: "0x11ce921929a81D3926E80AEc264F6e38A8685C07",
     infuraID: "045e9c65387a4200aa8559c347fa19a4",
     defaultGas: 125000,//75000,
@@ -15,17 +15,17 @@ module.exports = {
     debug: false,
     network: 'mainnet',
     chainId: 1,
-    pageHeader : 'FARM FOLK NFT',
+    pageHeader : 'Demon Squad NFT',
     pageText : 'Mint and collect the Rootinest, Tootinest NFT ever.',
     //mintType: "Whitelist",
     blockExplorerURL: 'https://.etherscan.io/',
-    www: 'https://FarmFolkNFT.com/',
-    mainWWW: 'https://FarmFolkNFT.com/',
-    title: 'FARM FOLK NFT',
+    www: 'https://DemonSquadNFT.com/',
+    mainWWW: 'https://DemonSquadNFT.com/',
+    title: 'Demon Squad NFT',
     links : {
       opensea : "https://opensea.io/collection/farm-folk-nft",
       discord : "https://discord.com/invite/hfyRXCuc",
-      twitter : "https://twitter.com/FarmFolkNFT"
+      twitter : "https://twitter.com/DemonSquadNFT"
     },
     maxMint: 3000,
     maxMintCount: 50,
